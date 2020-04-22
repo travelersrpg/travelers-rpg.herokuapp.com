c20aa6de8abb5a2dee49748c9595bd37280a156c
vagrant <vagrant@vagrant.vm>
2020-04-22 10:00:55 +0000

    p

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def home
     @news = Post.all
   end
+
+  def members
+    @news = Post.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get '/members', to:'pages#members'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
