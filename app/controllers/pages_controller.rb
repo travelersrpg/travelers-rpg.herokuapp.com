86c25cf5b5f2bff4853ea59593a02024f73e89ba
nino reetok <reetok3@gmail.com>
2020-04-22 19:31:56 +0900

    add image

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,21 @@
 class PagesController < ApplicationController
   def home
     @news = Post.all
+    @show_slideshow = true
   end
 
   def members
     @news = Post.all
+    @show_slideshow = false
+  end
+
+  def server_system
+    @news = Post.all
+    @show_slideshow = false
+  end
+
+  def story
+    @news = Post.all
+    @show_slideshow = false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   get '/members', to:'pages#members'
+  get '/server_sytem', to:'pages#server_sytem'
+  get '/story', to:'pages#story'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
