373dddf0889c55fde09e2ca5217869a8522bd5b5
nino reetok <reetok3@gmail.com>
2020-04-23 20:47:38 +0900

    fix test seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-User.create(name: "TestKun", email: "test@example.com", password: "test", password_confirmation: "test")
